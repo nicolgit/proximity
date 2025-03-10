b44b59bbb5e9e3bd59e51d138855cd105dd69dfc
Nicola Delfino <nicolgit@users.noreply.github.com>
2025-03-10 17:57:24 +0100

    npm react next.js scaffolding

---
+++ web-spa/src/app/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        metro proximity project. Please select a city to view the metro map.
+        
+        <div className={styles.ctas}>
+          <a
+            className={styles.secondary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rome
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+           Milan
+          </a>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        this is a footer
+      </footer>
+    </div>
+  );
+}
