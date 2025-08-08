906a00b076db8592897fb316da5ef7dd11f753fc
Nicola Delfino <nicolgit@users.noreply.github.com>
2025-08-08 13:30:01 +0200

    update ps1

--- video-source/build.ps1
+++ video-source/build.ps1
@@ -7,17 +7,31 @@ Remove-Item ../video/*.gif
 
 function Convert-JpegsToGif {
     param(
-        [string]$FileName,
+        [Parameter(Mandatory = $true)]
+        [string]$SourceDir,
+        [Parameter(Mandatory = $true)]
+        [string]$OutputDir,
+        [Parameter(Mandatory = $true)]
+        [string]$OutputFileName,
+        [Parameter(Mandatory = $true)]
         [float]$FramesPerSecond
     )
     
-    ffmpeg -framerate $FramesPerSecond -i $FileName/%02d.jpeg -c:v libvpx-vp9 -lossless 1 -vf "scale=1024:768" -y ../video/$FileName.mp4
+    $inputPattern = Join-Path $SourceDir "%02d.jpeg"
+    $outputMp4 = Join-Path $OutputDir "$OutputFileName.mp4"
+    $outputGif = Join-Path $OutputDir "$OutputFileName.gif"
+
+    echo xx $inputPattern
+    echo xx $outputMp4
+    echo xx $outputGif
+
+    ffmpeg -framerate $FramesPerSecond -i $inputPattern -c:v libvpx-vp9 -lossless 1 -vf "scale=1024:768" -y $outputMp4
     $palette = "palette.png"
     $filters = "fps=12,scale=1072:-1:flags=lanczos"
-    ffmpeg -v warning -i ../video/$FileName.mp4 -vf "$filters,palettegen" -y $palette
-    ffmpeg -v warning -i ../video/$FileName.mp4 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y ../video/$FileName.gif
+    ffmpeg -v warning -i $outputMp4 -vf "$filters,palettegen" -y $palette
+    ffmpeg -v warning -i $outputMp4 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $outputGif
     Remove-Item $palette
-    Remove-Item ../video/$FileName.mp4
+    Remove-Item $outputMp4
 }
 
-Convert-JpegsToGif -FileName "welcome" -FramesPerSecond 0.5
+Convert-JpegsToGif -SourceDir "welcome" -OutputDir "../video" -OutputFileName "welcome" -FramesPerSecond 0.5
