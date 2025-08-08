24c6c96b3fd2d2338f2c04e08a2ff24e08d75d8c
Nicola Delfino <nicolgit@users.noreply.github.com>
2025-08-08 12:57:22 +0200

    build animated gif

--- video-source/build.ps1
+++ video-source/build.ps1
@@ -3,5 +3,21 @@ winget install --id=Gyan.FFmpeg
 Clear-Host
 
 Remove-Item ../video/*.mp4 
+Remove-Item ../video/*.gif 
 
-ffmpeg -framerate 0.5 -i welcome/%02d.jpeg -c:v libvpx-vp9 -lossless 1 -y ../video/welcome.mp4
+function Convert-JpegsToGif {
+    param(
+        [string]$FileName,
+        [float]$FramesPerSecond
+    )
+    
+    ffmpeg -framerate $FramesPerSecond -i $FileName/%02d.jpeg -c:v libvpx-vp9 -lossless 1 -vf "scale=1024:768" -y ../video/$FileName.mp4
+    $palette = "palette.png"
+    $filters = "fps=12,scale=1072:-1:flags=lanczos"
+    ffmpeg -v warning -i ../video/$FileName.mp4 -vf "$filters,palettegen" -y $palette
+    ffmpeg -v warning -i ../video/$FileName.mp4 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y ../video/$FileName.gif
+    Remove-Item $palette
+    Remove-Item ../video/$FileName.mp4
+}
+
+Convert-JpegsToGif -FileName "welcome" -FramesPerSecond 0.5
